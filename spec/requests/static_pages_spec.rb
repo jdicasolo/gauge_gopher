d158dee5e1a57c98cecdf37b160fbd73bfff80f7
jeremy <jdicasolo@gmail.com>
2012-10-09 09:37:09 -0400

    static pages stubbed out

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -3,24 +3,45 @@ require 'spec_helper'
 describe "StaticPages" do
 
   describe "Home Page" do
+    
     it "should contain the content 'Gauge Gopher | Home'" do
       visit '/static_pages/home'
       page.should have_content('Gauge Gopher | Home')
     end
+    
+    it "should have a title 'Home'" do
+      visit '/static_pages/home'
+      page.should have_selector('title', :text => "Gauge Gopher | Home")
+    end
+    
   end
 
   describe "Help Page" do
+    
     it "should contain the content 'Gauge Gopher | Help'" do
       visit '/static_pages/help'
       page.should have_content('Gauge Gopher | Help')
     end
+    
+    it "should have a title 'Help'" do
+      visit '/static_pages/help'
+      page.should have_selector('title', :text => "Gauge Gopher | Help")
+    end
+
   end
 
   describe "About Page" do
+
     it "should contain the content 'Gauge Gopher | About'" do
       visit '/static_pages/about'
       page.should have_content('Gauge Gopher | About Us')
     end
+
+    it "should have a title 'About'" do
+      visit '/static_pages/about'
+      page.should have_selector('title', :text => "Gauge Gopher | About")
+    end
+
   end
   
 end
